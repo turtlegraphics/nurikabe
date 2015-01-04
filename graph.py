90b9517603b6cab61f87eca2a31a878be644e685
Bryan Clair <bryan@slu.edu>
2015-01-04 16:05:21 -0600

    graph module with graph utils: find_shortest_cycles

---
+++ graph.py
@@ -0,0 +1,83 @@
+"""
+Graph utility algorithms
+"""
+
+import networkx as nx
+
+def find_shortest_cycles(g):
+    """
+    Return a pair (girth, d)
+    where d is a dictionary keyed by nodes, each entry of which
+    has a list of the shortest cycles containing the key node.
+    If graph is a tree, raises a ValueError.
+    """
+    cycles = []
+    # start with all possible length 0 paths
+    girth = 0
+    paths =[[n] for n in g]
+    closed = False
+    tree = False
+    while not closed and not tree:
+        # detect trees
+        tree = True
+        # build all paths that are one step longer
+        newpaths = []
+        girth += 1
+        for p in paths:
+            head = p[0]
+            try:
+                last = p[1]
+            except IndexError:
+                last = None
+
+            # loop through neighbors of the head of path, making new paths
+            for nbr in g[p[0]]:
+                # don't backtrack
+                if nbr == last:
+                    continue
+                tree = False # don't quit yet - paths still growing
+                # check if cycle closed
+                if nbr == p[-1]:
+                    closed = True
+                    newpaths.append(p)
+                else:
+                    newp = [nbr]
+                    newp.extend(p)
+                    newpaths.append(newp)
+        paths = newpaths
+
+    if tree:
+        raise ValueError('graph is a tree')
+
+    # pick only the closed paths (which have the correct length)
+    # then eliminate duplicates by making into a set
+    cycles = set([frozenset(c) for c in paths if len(c)==girth])
+
+    # build a dictionary of cycles keyed by nodes
+    d = {}
+    for n in g:
+        d[n] = [c for c in cycles if n in c]
+
+    return (girth,d)
+
+if __name__=='__main__':
+    g = nx.grid_graph([4,4])
+    (girth,d) = find_shortest_cycles(g)
+    print '4x4 grid has girth',girth,'and nodes have this many short cycles:'
+    for y in range(4):
+        for x in range(4):
+            print len(d[(x,y)]),
+        print
+
+    g = nx.complete_graph(5)
+    (girth,d) = find_shortest_cycles(g)
+    print 'K5 has girth',girth
+    for n in g:
+        print n,':',[list(c) for c in d[n]]
+
+    g = nx.balanced_tree(3,4)
+    print '3-regular tree with %d nodes' % len(g)
+    try:
+        find_shortest_cycles(g)
+    except ValueError as msg:
+        print 'Error:',msg
