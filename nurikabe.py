b5c2099e5bd0ed6c073983eecbc3154d408fe995
Bryan Clair <bryan@slu.edu>
2015-01-05 01:32:43 -0600

    Handle output of anchors size >= 10

--- nurikabe.py
+++ nurikabe.py
@@ -14,6 +14,7 @@ where example.txt might contain
 
 """
 import sys
+import time
 import solver
 import board
 
@@ -24,10 +25,14 @@ print 'Board:'
 print b
 print
 print 'Solutions:'
+start = time.time()
 answers = solver.Solver(b).solve()
+end = time.time()
 if answers:
     for a in answers:
         print a
         print
 else:
     print 'None\n'
+
+print end-start,'elapsed.'
