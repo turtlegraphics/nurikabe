a80d25d239aba9499408bb875e83e590cf0996ed
Bryan Clair <bryan@slu.edu>
2015-01-04 18:49:39 -0600

    Anonymous land regions.

--- regions.py
+++ regions.py
@@ -6,10 +6,9 @@ import networkx as nx
 
 class Region:
     """Track a region of the board."""
-    def __init__(self,board):
+    def __init__(self):
         """Requires a board."""
         self.nodes = []
-        self.board = board
 
     def add_node(self,node):
         assert node not in self.nodes
@@ -23,37 +22,32 @@ class Region:
 
 class Water(Region):
     """Track a water region of the board."""
-    def __init__(self,board):
-        Region.__init__(self,board)
+    def __init__(self):
+        Region.__init__(self)
         self.style = '#'
 
     def __str__(self):
         return 'Water, %s.' % Region.__str__(self)
 
-    def would_pool(self,node):
-        """Determine if the node would create a pool."""
-        possiblepools = self.board.pools[node]
-        for p in possiblepools:
-            ispool = True
-            for n in p:
-                if n == node:
-                    continue
-                if n not in self.nodes:
-                    ispool = False
-            if ispool:
-                return True
-        return False
+class Land(Region):
+    """Track an anonymous land region of the board."""
+    def __init__(self):
+        Region.__init__(self)
+        self.style = '*'
 
+    def __str__(self):
+        return 'Land, %s.' % Region.__str__(self)
+    
 class Island(Region):
     """Track an island region of the board."""
 
     # An iterator to generate different 'styles' for different regions
     newstyle = itertools.cycle(string.lowercase).next
 
-    def __init__(self,board,anchor,size):
+    def __init__(self,anchor,size):
         """Create with an anchor node and desired size,
            or use anchor of None for the water."""
-        Region.__init__(self,board)
+        Region.__init__(self)
         self.anchor = anchor
         self.size = size
         self.style = Island.newstyle()
@@ -86,9 +80,9 @@ class Island(Region):
             str(self.style), str(self.anchor), self.size, Region.__str__(self))
 
 if __name__=='__main__':
-    w = Water(None)
-    r1 = Island(None,(1,0),4)
-    r2 = Island(None,(2,2),2)
+    w = Water()
+    r1 = Island((1,0),4)
+    r2 = Island((2,2),2)
     r2.add_node((2,3))
 
     print 'w: ',w
